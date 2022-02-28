a4ee6c1af127be17a2a88c82bc3900fb649e0795
ilya0 <iosovets@gmail.com>
2022-02-28 15:34:39 -0800

    folder

---
+++ my-wave-portal/artifacts/contracts/WavePortal.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0; //tell what version of the compiler to use 
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor(){
+        console.log("Yo yo, I ama contract and I am smart");
+    }
+}
